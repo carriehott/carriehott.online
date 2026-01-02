94c86b63a75245e36b43f141bab0e6f0f67c1028
carriehott <carriehott@gmail.com>
2026-01-02 16:14:06 -0500

    upload two

---
+++ .eleventy.js
@@ -0,0 +1,17 @@
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("src/assets/"); 
+    eleventyConfig.addPassthroughCopy("src/css/"); 
+    eleventyConfig.addWatchTarget("src/css/");
+    eleventyConfig.addPassthroughCopy("src/js");
+  return {
+    dir: {
+      input: 'src',
+      includes: '_includes',
+      output: '_site',
+    },
+    templateFormats: ['md', 'njk', 'html'],
+    markdownTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+    dataTemplateEngine: 'njk',
+  };
+}
