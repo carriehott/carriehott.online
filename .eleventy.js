49c28a33eae74a7e2903fdd60aae6354694e3345
carriehott <carriehott@gmail.com>
2026-02-08 16:49:19 -0500

    updates

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,7 @@
+const rssPlugin = require('@11ty/eleventy-plugin-rss');
+
 module.exports = function(eleventyConfig) {
+    eleventyConfig.addPlugin(rssPlugin);
     eleventyConfig.addPassthroughCopy("src/assets/"); 
     eleventyConfig.addPassthroughCopy("src/css/"); 
     eleventyConfig.addWatchTarget("src/css/");
